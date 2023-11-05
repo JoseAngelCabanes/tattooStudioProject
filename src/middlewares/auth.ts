328476def1c914dd21b787c951826509d608916b
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2023-11-06 00:41:37 +0100

    add auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const auth = (req: any, res: Response, next: NextFunction) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.json(
+        {
+          message: 'AUTH_REQUIRED'
+        }
+      )
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+      return res.json(
+        {
+          message: 'AUTH_REQUIRED'
+        }
+      )
+    }
+
+    const tokenDecoded = jwt.verify(token, "secreto")
+
+    req.token = tokenDecoded
+    
+    next()
+  } catch (error) {
+    return res.json({
+      error: "Not auth"
+    })
+  }
+}
+
+export { auth }
