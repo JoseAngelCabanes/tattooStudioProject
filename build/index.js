67e6af34e78ddd50a2868dfb3370f243f369c86d
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2023-11-06 00:09:11 +0100

    add types folder with token decoded

---
+++ build/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const db_1 = require("./db");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const PORT = process.env.PORT || 3000;
+// app.use('/client', routerCustomers)
+// app.use('/artist', routerTatoo_artists)
+// app.use('/appointment', routerAppointment)
+// app.use('/portfolio', routerPortfolio)
+db_1.AppDataSource.initialize()
+    .then(() => {
+    console.log('Database connected');
+    app.listen(PORT, () => {
+        console.log(`Server running ${PORT}`);
+    });
+})
+    .catch(error => {
+    console.log(error);
+});
---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+export {};
+export interface TokenDecoded {
+  id: number;
+  role: string;
+  email: string;
+}
+declare global {
+  namespace Express {
+    export interface Request {
+      // decoded token
+      token: TokenDecoded;
+    }
+  }
+}
