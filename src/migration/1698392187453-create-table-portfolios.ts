360282a13099b0bf01a6175a94be8c4968548770
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2023-11-06 01:36:14 +0100

    add migrations

---
+++ src/migration/1698392187453-create-table-portfolios.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateTablePortfolios1698392187453 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "portfolios",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "artist_id",
+                        type: "int"
+                    },
+                    {
+                        name: "image",
+                        type: "varchar",
+                        length: "255"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",                        
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                        onUpdate: "CURRENT_TIMESTAMP"                 
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["artist_id"],
+                        referencedTableName: "artists",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE",
+                    }
+                ]
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("portfolios");
+    }
+
+}
